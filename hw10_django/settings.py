3ca9e9ade29049c510eefa4cdead9bcde4722cca
Serhii Kroshka <pm012@ukr.net>
2024-05-26 21:49:00 +0300

    Added settings and routes for password reset

--- hw10_django/settings.py
+++ hw10_django/settings.py
@@ -133,3 +133,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.meta.ua'
+EMAIL_PORT = 465
+EMAIL_STARTTLS = False
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = 'serhii.kroshka@meta.ua'
+EMAIL_HOST_PASSWORD = 'TmpTmp1@'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
